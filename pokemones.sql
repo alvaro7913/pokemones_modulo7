5336048a322fdecb7329c7be22af347407fc0b1c
alvaro7913 <alvarocesped@gmail.com>
2022-04-11 20:34:17 -0400

    octavo commit

--- pokemones.sql
+++ pokemones.sql
@@ -34,9 +34,22 @@ SELECT pokedex AS número FROM pokemones WHERE nombre = 'Eevee';
 
 NSERT INTO mis_pokemones (pokedex, fecha_captura, lugar, huevo, peso, estatura)
 VALUES (133, '2021-04-12', 'Puente Asalto', false, 15.6, 0.3);
-NSERT INTO mis_pokemones (pokedex, fecha_captura, lugar, huevo, peso, estatura)
+INSERT INTO mis_pokemones (pokedex, fecha_captura, lugar, huevo, peso, estatura)
 VALUES (134, '2021-04-12', 'La loma del toro', false, 25.6, 0.8);
 
 SELECT * FROM mis_pokemones ORDER BY fecha_captura DESC LIMIT 3;
 SELECT * FROM mis_pokemones WHERE EXTRACT (YEAR FROM fecha_captura) = 2021;
 SELECT SUM(peso) AS total FROM mis_pokemones;
+
+ALTER TABLE pokemones
+ADD rockets BOOLEAN;
+SELECT * FROM pokemones LIMIT 1;
+
+INSERT INTO pokemones (pokedex, nombre, tipo1, tipo2, rocket)
+VALUES (152,'pichu', 'electrico','', false);
+
+SELECT * FROM pokemones ORDER BY pokedex DESC LIMIT 1;
+
+UPDATE pokemones SET rocket = false;
+
+SELECT rocket AS capturado_por_los_malos FROM pokemones LIMIT 2;
